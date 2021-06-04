0aab175b2b2d016ca74b48670f4b1dcec650409a
Vladimir-Okulovich <vladimir.okulovich2019@mail.ru>
2021-06-04 16:12:25 +0200

    auth

--- resources/js/common/config.js
+++ resources/js/common/config.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// export const API_URL = 'http://192.168.0.18:8000';
-export const API_URL = 'http://10.10.11.62:8001';
-=======
 export const API_URL = 'http://192.168.0.18:8000';
 // export const API_URL = 'http://10.10.11.62:8000';
->>>>>>> d45b99762b3e7e9e8417ac4a962ae84ae373d1e5
 export default API_URL;
